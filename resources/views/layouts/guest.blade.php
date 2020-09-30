527c98b6e6c9a3e8aa27eb912254d71d3f13ef99
AMACALAWI <amacalawi@gmail.com>
2020-09-30 13:23:58 +0800

    hotfix-30-09-2020-01

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -64,9 +64,15 @@
 							$.each(res.data[0], function (k, v) {
 								if (k == 'stud_no') {
 									$('#custom-pic img').attr('src', path + '/' + v + '.jpg');
+									$('#custom-pic img').error(function(){
+										$(this).attr('src', path + '/' + v + '.JPG');
+									});
 								}
                                 $('body').find('span[name=' + k + ']').text(v);
                             });
+							setTimeout(function(){ 
+								location.replace("http://dioceseofantipolo.net/e/position/nsdaps/4");
+							}, 4000);
 						} else {
 							$('#try_again')[0].play();
 							$('#preview').addClass("hidden");
