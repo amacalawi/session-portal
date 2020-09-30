77f4e1dfdbd71124b1ddf7d1d21c7d21674bdfbd
root <amacalawi@gmail.com>
2020-09-30 15:30:07 +0800

    hotfix-30-09-2020-02-03

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -64,7 +64,6 @@
 							$.each(res.data[0], function (k, v) {
 								if (k == 'stud_no') {
 									$('#custom-pic img').attr('src', path + '/' + v + '.jpg');
-									// check if image is exist
 									var image = new Image(); 
 									image.src = path + '/' + v + '.jpg';
 									if (image.width == 0) {
@@ -73,9 +72,9 @@
 								}
                                 $('body').find('span[name=' + k + ']').text(v);
                             });
-							// setTimeout(function(){ 
-							// 	location.replace("http://dioceseofantipolo.net/e/position/nsdaps/4");
-							// }, 4000);
+							setTimeout(function(){ 
+								location.replace("http://dioceseofantipolo.net/e/position/nsdaps/4");
+							}, 4000);
 						} else {
 							$('#try_again')[0].play();
 							$('#preview').addClass("hidden");
